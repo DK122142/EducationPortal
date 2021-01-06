7906519e64e7cb909252be08b16cf2082b9141a2
d3c6e1 <dmytro.koval@nure.ua>
2021-01-06 15:54:00 +0200

    user entity

---
+++ EducationPortal/Entities/Entity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace EducationPortal.Entities
+{
+    public abstract class Entity
+    {
+        [JsonInclude]
+        public Guid Id { get; }
+
+        public Entity(Guid id)
+        {
+            Id = id;
+        }
+    }
+}
---
+++ EducationPortal/Entities/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using EducationPortal.Services;
+
+namespace EducationPortal.Entities
+{
+    public class User : Entity
+    {
+        [JsonInclude]
+        public string Name { get; private set; }
+
+        [JsonInclude]
+        public string Password { get; private set; }
+
+        public User(Guid id, string name, string password) : base(id)
+        {
+            Name = name;
+            Password = password;
+        }
+    }
+}
