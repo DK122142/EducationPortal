818bbe8c3b21e522c6cc74ce6d815810e177e424
d3c6e1 <dmytro.koval@nure.ua>
2021-01-08 11:58:48 +0200

    bulls and cows

---
+++ EducationPortal/AdditionalTasks/BullsAndCows/Service.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EducationPortal.AdditionalTasks.BullsAndCows
+{
+    public static class Service
+    {
+        public static List<string> GenerateCombinations()
+        {
+            List<string> generatedList = new List<string>();
+
+            for (int i = 123; i <= 9876; i++) {
+                int d1 = i / 1000;
+                int d2 = (i / 100) % 10;
+                int d3 = (i / 10) % 10;
+                int d4 = i % 10;
+               
+                if (d1 != d2 && d1 != d3 && d1 != d4 && d2 != d3 && d2 != d4 && d3 != d4) {
+                    generatedList.Add(string.Join("", i));
+                }
+            }
+            return generatedList;
+        }
+    }
+}
--- EducationPortal/Program.cs
+++ EducationPortal/Program.cs
@@ -1,13 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text.Json;
-using System.Threading.Tasks;
-using EducationPortal.Controllers;
-using EducationPortal.Entities;
-using EducationPortal.Services;
-using EducationPortal.Storage;
-using EducationPortal.UI.Components;
-using static System.Int32;
+using EducationPortal.AdditionalTasks.BullsAndCows;
+using EducationPortal.AdditionalTasks.CSVGenerator;
 
 namespace EducationPortal
 {
@@ -15,59 +8,19 @@ namespace EducationPortal
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Education Portal");
+            
+            Console.WriteLine("1. Bulls and Cows");
+            Console.WriteLine("2. CSV generator");
 
-            Console.WriteLine("1. Log In");
-            Console.WriteLine("2. Register");
+            var option = int.Parse(Console.ReadLine());
 
-            var choice = Parse(Console.ReadLine());
-
-            string login;
-            string password;
-
-            AuthController ac = new AuthController();
-
-            User user;
-
-            switch (choice)
+            switch (option)
             {
                 case 1:
-                    Console.WriteLine("Log In");
-                    Console.WriteLine("Input login");
-                    login = Console.ReadLine();
-                    Console.WriteLine("Input password");
-                    password = Console.ReadLine();
-
-                    user = ac.Login(login, password).Result;
-
-                    if (user != null)
-                    {
-                        Console.WriteLine($"Logined! Name: {user.Name}");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Something went wrong");
-                    }
-
+                    BullsCows.Start();
                     break;
                 case 2:
-                    Console.WriteLine("Registration");
-                    Console.WriteLine("Input login");
-                    login = Console.ReadLine();
-                    Console.WriteLine("Input password");
-                    password = Console.ReadLine();
-
-                    user = ac.Register(login, password).Result;
-
-                    if (user != null)
-                    {
-                        Console.WriteLine($"Registered! Name: {user.Name}");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Something went wrong");
-                    }
-
+                    CSVGenerator.Start();
                     break;
                 default:
                     Console.WriteLine("Wrong option");
