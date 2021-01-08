44865621f56305bd47c2ee5ad0aa963e61045d93
d3c6e1 <dmytro.koval@nure.ua>
2021-01-08 12:33:35 +0200

    csv file generation by console input field names

---
+++ EducationPortal/AdditionalTasks/CSVGenerator/Service.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+
+namespace EducationPortal.AdditionalTasks.CSVGenerator
+{
+    public class Service
+    {
+        public static List<string> GetFields()
+        {
+            var fields = typeof(Person).GetFields();
+            var result = new List<string>();
+
+            foreach (var field in fields)
+            {
+                result.Add(field.Name);
+            }
+
+            return result;
+        }
+
+        public static List<string> SplitFieldNames(string fields)
+        {
+            var fieldNames = fields.Trim().Split(",");
+            var result = new List<string>();
+
+            foreach (var fieldName in fieldNames)
+            {
+                result.Add(fieldName.Trim().ToLower());
+            }
+            
+            return result;
+        }
+
+        public static string ValueByFieldName (Person instance, string field, List<string> fields)
+        {
+            if (fields.Contains(field))
+            {
+                return typeof(Person).GetField(field).GetValue(instance).ToString();
+            }
+
+            return default;
+        }
+
+        public static List<string> ToCSVRows(List<List<string>> list)
+        {
+            var result = new List<string>();
+
+            foreach (var subList in list)
+            {
+                result.Add(string.Join(", ", subList));
+            }
+
+            return result;
+        }
+    }
+}
