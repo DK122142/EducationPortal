2d109e24a47cb797b5c28f1e41c64788b53f8984
d3c6e1 <dmytro.koval@nure.ua>
2021-01-15 15:46:12 +0200

    added interfaces for storage

---
+++ EducationPortal/Storage/IStorageController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EducationPortal.Storage
+{
+    public interface IStorageController
+    {
+        Storage Storage { get; }
+
+        IStorageController CreateTable<T>();
+
+        IStorageController DeleteTable<T>();
+        
+        Task InsertInto<T>(IEnumerable<T> values);
+
+        Task InsertInto<T>(T value);
+
+        Task Update<T>(Guid id, T value);
+    }
+}
