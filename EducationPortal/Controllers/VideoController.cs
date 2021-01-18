804df217fa4385e6d43df23bb0ae7852275c62e9
DmitriyK42 <dmytro.koval00@gmail.ua>
2021-01-18 14:28:41 +0200

    video controller

---
+++ EducationPortal/Controllers/VideoController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using EducationPortal.Entities;
+using EducationPortal.Storage;
+
+namespace EducationPortal.Controllers
+{
+    public class VideoController : MaterialController
+    {
+        public VideoController(ITableManager tableManager) : base(tableManager)
+        {
+            TableManager.StorageController.CreateTable<Video>();
+        }
+
+        public async Task<Video> AddVideo(string name, Guid owner, string link, string duration, string quality)
+        {
+            var video = new Video(Guid.NewGuid(), name, owner, link, duration, quality);
+
+            if (await AddMaterial(video) == null)
+            {
+                Console.WriteLine($"Video with name {video.Name} already exists!");
+                return null;
+            }
+
+            return video;
+        }
+    }
+}
