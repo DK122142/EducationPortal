f0ca7c925b56f3297f2566fc4c4a9795b9744d90
DmitriyK42 <dmytro.koval00@gmail.ua>
2021-01-29 10:49:42 +0200

    views

---
+++ EducationPortal.Prompt/Views/Home/Home.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using EducationPortal.Prompt.Infrastructure;
+using EducationPortal.Prompt.Views.Account;
+using EducationPortal.Prompt.Views.Shared;
+
+namespace EducationPortal.Prompt.Views.Home
+{
+    public static class Home
+    {
+        public static void Show(string msg = null)
+        {
+            Console.Clear();
+            Header.Show();
+            
+            if (msg != null)
+            {
+                Console.WriteLine("========");
+                Console.WriteLine($"message: {msg}");
+                Console.WriteLine("========");
+            }
+            
+            if (Provider.AuthorizedUser == null)
+            {
+                Login.Show(msg);
+            }
+            else
+            {
+                Profile.Show(msg);
+            }
+        }
+    }
+}
---
+++ EducationPortal.Prompt/Views/Home/Login.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using EducationPortal.Prompt.Infrastructure;
+using EducationPortal.Prompt.Models;
+using EducationPortal.Prompt.Views.Shared;
+
+namespace EducationPortal.Prompt.Views.Home
+{
+    public class Login
+    {
+        public static void Show(string msg = null)
+        {
+            string login;
+            string password;
+
+            Console.Clear();
+            Header.Show();
+            
+            if (msg != null)
+            {
+                Console.WriteLine("========");
+                Console.WriteLine($"message: {msg}");
+                Console.WriteLine("========");
+            }
+
+            Console.WriteLine("Login.");
+            Console.WriteLine("(1)Log In. \n" +
+                              "Not registered yet? (2)Registration. \n" +
+                              "(5)Exit");
+            
+            int.TryParse(Console.ReadLine(), out var option);
+
+            Console.Clear();
+
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Input your login: ");
+                    login = Console.ReadLine();
+
+                    Console.WriteLine("Input your password: ");
+                    password = Console.ReadLine();
+
+                    Provider.AccountController.Login(new LoginModel
+                    {
+                        Login = login,
+                        Password = password
+                    });
+
+                    break;
+                case 2:
+                    Registration.Show();
+                    break;
+                case 5:
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Wrong option");
+                    break;
+            }
+
+            Console.Clear();
+
+            Home.Show();
+        }
+    }
+}
