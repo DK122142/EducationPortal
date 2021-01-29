124958527146b70b5558db9d314b327fd4293957
DmitriyK42 <dmytro.koval00@gmail.ua>
2021-01-29 10:48:53 +0200

    controllers

---
+++ EducationPortal.Prompt/Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using System.Threading.Tasks;
+using EducationPortal.BLL.DTO;
+using EducationPortal.BLL.Interfaces;
+using EducationPortal.Prompt.Infrastructure;
+using EducationPortal.Prompt.Models;
+using EducationPortal.Prompt.Views.Home;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EducationPortal.Prompt.Controllers
+{
+    public class AccountController
+    {
+        private IAccountService AccountService => AppContext.ServiceProvider.GetService<IAccountService>();
+
+        public void Login(LoginModel model)
+        {
+            AccountDTO accountDto = new AccountDTO {Login = model.Login, Password = model.Password};
+            var authAcc = this.AccountService.Authenticate(accountDto);
+
+            if (authAcc != null)
+            {
+                Logout();
+
+                Provider.AuthorizedUser = new AccountDTO
+                {
+                    Id = authAcc.Id,
+                    Login = authAcc.Login,
+                    Password = authAcc.Password
+                };
+            }
+
+            Home.Show();
+        }
+
+        public async Task Register(LoginModel model)
+        {
+            AccountDTO accountDto = new AccountDTO {Login = model.Login, Password = model.Password};
+            var operationDetails = await this.AccountService.Create(accountDto);
+
+            if (operationDetails.Succeded)
+            {
+                Login(model);
+            }
+
+            Home.Show($"{operationDetails.Message}, {operationDetails.Property}");
+        }
+
+        public void Logout()
+        {
+            Provider.AuthorizedUser = null;
+        }
+    }
+}
---
+++ EducationPortal.Prompt/Infrastructure/Provider.cs
@@ -0,0 +1,12 @@
+﻿using EducationPortal.BLL.DTO;
+using EducationPortal.Prompt.Controllers;
+
+namespace EducationPortal.Prompt.Infrastructure
+{
+    public static class Provider
+    {
+        public static AccountDTO AuthorizedUser { get; set; } = default;
+
+        public static AccountController AccountController { get; set; } = new AccountController();
+    }
+}
