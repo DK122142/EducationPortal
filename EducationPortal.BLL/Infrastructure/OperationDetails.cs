efbad799296c9741afcb86480d958d080dae8cc1
DmitriyK42 <dmytro.koval00@gmail.ua>
2021-01-29 10:46:58 +0200

    services

---
+++ EducationPortal.BLL/Infrastructure/OperationDetails.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EducationPortal.BLL.Infrastructure
+{
+    public class OperationDetails
+    {
+        public bool Succeded { get; private set; }
+
+        public string Message { get; private set; }
+
+        public string Property { get; private set; }
+
+        public OperationDetails(bool succedeed, string message, string prop)
+        {
+            this.Succeded = succedeed;
+            this.Message = message;
+            this.Property = prop;
+        }
+    }
+}
---
+++ EducationPortal.BLL/Interfaces/IAccountService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using EducationPortal.BLL.DTO;
+using EducationPortal.BLL.Infrastructure;
+using EducationPortal.DAL.Entities;
+
+namespace EducationPortal.BLL.Interfaces
+{
+    public interface IAccountService : IDisposable
+    {
+        Task<OperationDetails> Create(AccountDTO accountDto);
+
+        Account Authenticate(AccountDTO userDto);
+    }
+}
