bd6f932f1f59b2cf2d7c66b1c3aec287db271df8
DmitriyK42 <dmytro.koval00@gmail.ua>
2021-01-29 15:52:43 +0200

    bll services

---
+++ EducationPortal.BLL/Interfaces/IVideoService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EducationPortal.BLL.DTO;
+
+namespace EducationPortal.BLL.Interfaces
+{
+    public interface IVideoService
+    {
+        void AddVideo(VideoDTO video);
+
+        VideoDTO GetVideo(Guid id);
+    }
+}
---
+++ EducationPortal.BLL/Services/VideoService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EducationPortal.BLL.DTO;
+using EducationPortal.BLL.Interfaces;
+using EducationPortal.DAL.Entities;
+using EducationPortal.DAL.Interfaces;
+
+namespace EducationPortal.BLL.Services
+{
+    public class VideoService : IVideoService
+    {
+        private IUnitOfWork Db { get; set; }
+
+        public VideoService(IUnitOfWork uow)
+        {
+            this.Db = uow;
+        }
+
+        public void AddVideo(VideoDTO video)
+        {
+            Video newVideo = new Video(Guid.NewGuid(), video.Name, video.Owner, video.Link, video.Duration,
+                video.Quality);
+
+            this.Db.Videos.Create(newVideo);
+        }
+
+        public VideoDTO GetVideo(Guid id)
+        {
+            Video video = this.Db.Videos.Get(id);
+
+            if (video == null)
+            {
+                return null;
+            }
+
+            return new VideoDTO
+            {
+                Id = video.Id,
+                Name = video.Name,
+                Owner = video.Owner,
+                Duration = video.Duration,
+                Link = video.Duration,
+                Quality = video.Quality
+            };
+        }
+    }
+}
