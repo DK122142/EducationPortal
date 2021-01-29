aededfb757d24a9b8ab4bbce753680ba4b06898f
DmitriyK42 <dmytro.koval00@gmail.ua>
2021-01-29 15:53:52 +0200

    models

---
+++ EducationPortal.Prompt/Models/MaterialModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EducationPortal.Prompt.Models
+{
+    public abstract class MaterialModel : EntityModel
+    {
+        public string Name { get; set; }
+
+        public Guid Owner { get; set; }
+    }
+}
---
+++ EducationPortal.Prompt/Models/VideoModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EducationPortal.Prompt.Models
+{
+    public class VideoModel : MaterialModel
+    {
+        public string Link { get; set; }
+
+        public string Duration { get; set; }
+
+        public string Quality { get; set; }
+    }
+}
