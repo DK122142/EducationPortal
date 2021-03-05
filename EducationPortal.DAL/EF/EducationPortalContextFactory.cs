116a92c86e55654b886cf77d5a840e8fa4d0d9ab
DmitriyK42 <dmitriykoval00@gmail.com>
2021-03-05 17:58:57 +0200

    migration fix

---
+++ EducationPortal.DAL/EF/EducationPortalContextFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace EducationPortal.DAL.EF
+{
+    public class EducationPortalContextFactory : IDesignTimeDbContextFactory<EducationPortalContext>
+    {
+        public EducationPortalContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<EducationPortalContext>();
+            var builder = new ConfigurationBuilder();
+
+            builder.SetBasePath(Directory.GetCurrentDirectory());
+            builder.AddJsonFile("appsettings.json");
+
+            IConfigurationRoot config = builder.Build();
+
+            var connectionString = config.GetConnectionString("DefaultConnection");
+
+            optionsBuilder.UseSqlServer(connectionString,
+                opts => opts.CommandTimeout((int) TimeSpan.FromMinutes(10).TotalSeconds));
+
+            return new EducationPortalContext(optionsBuilder.Options);
+        }
+    }
+}
