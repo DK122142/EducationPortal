9f80ba26f462f02aa200df155550bcb25f0442c9
DmitriyK42 <dmitriykoval00@gmail.com>
2021-03-18 20:52:22 +0200

    fix login/register validation

--- EducationPortal.WEB.MVC/Validation/LoginViewModelValidator.cs
+++ EducationPortal.WEB.MVC/Validation/LoginViewModelValidator.cs
@@ -10,14 +10,12 @@ namespace EducationPortal.WEB.MVC.Validation
             RuleFor(x => x.Login)
                 .NotEmpty()
                 .MinimumLength(4)
-                .MaximumLength(40)
-                .WithMessage("Username length must be from 4 to 40");
+                .MaximumLength(40);
 
-            RuleFor(x=>x.Password)
+            RuleFor(x => x.Password)
                 .NotEmpty()
                 .MinimumLength(4)
-                .MaximumLength(40)
-                .WithMessage("Password length must be from 4 to 40");
+                .MaximumLength(40);
 
         }
     }
